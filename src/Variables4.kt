a85a84fc28b8ba7f62a7998194eaeb73f2b3b226
Georgi Naumov <gonaumov@gmail.com>
2019-12-30 01:15:08 +0200

    More examples. Adding another top level variable.

---
+++ src/Variables4.kt
@@ -0,0 +1,10 @@
+val name4: String = "Nate"
+var greeting: String = "hello"
+fun main() {
+    println(greeting)
+    println(name4)
+
+    greeting = "hi"
+    println(greeting)
+    println(name4)
+}
